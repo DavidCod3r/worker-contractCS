0e954715b6895b7fc079770cae0308710e365ff5
David Almeida <davidalmeidadrive@gmail.com>
2020-05-25 15:39:16 -0300

    first

---
+++ Worker/Entities/Enums/WorkerLevel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Worker.Entities.Enums
+{
+    enum WorkerLevel
+    {
+        JUNIOR,
+        MID_LEVEL,
+        SENIOR
+    }
+}
---
+++ Worker/Entities/Worker.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Text;
+using Worker.Entities.Enums;
+
+namespace Worker.Entities
+{
+    class Worker
+    {
+        public string Name { get; private set; }
+        public WorkerLevel Level { get; private set; }
+        public double  BaseSalary { get; private set; }
+    
+        private List<HourContract> contracts = new List<HourContract>();
+        private Department Department;
+
+
+        public void AddContracts(HourContract contract)
+        {
+            contracts.Add(contract);
+        }
+
+        public void RemoveContracts(HourContract contract)
+        {
+            contracts.Remove(contract);
+        }
+
+        public double Income(int month, int year)
+        {
+            return 0;
+        }
+
+    }
+}
